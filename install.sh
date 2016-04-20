47cfc710937a0ba67874f579c9e55431181817b2
Lior Rozen <lixroz+github@gmail.com>
2016-04-20 18:59:59 +0300

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Download vim-plug & creare appropriate folders
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Open vim, run :PlugInstall and quit
+vim +PlugInstall +qall
+
+# Go to the YouCompleteMe folder
+cd ~/.vim/plugged/YouCompleteMe/ 
+
+# Execute the YouCompleteMe install script
+./install.py --clang-completer --gocode-completer --tern-completer
